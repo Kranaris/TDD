44c0a27e194ce6f75e77d79c3a2d8d794244ef78
Nikita Karavaev <nikitakaravaev@mail.ru>
2023-09-02 22:56:25 +0300

    Модель для элементов списка и связанная с ней миграция

--- lists/views.py
+++ lists/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    '''домашняя страница'''
+    """домашняя страница"""
     return render(request, 'home.html', {
         'new_item_text': request.POST.get('item_text', ''),
     })
