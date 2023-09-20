3de88b4047c7641115a2dd0993528d2b62076bd7
Nikita Karavaev <nikitakaravaev@mail.ru>
2023-09-20 10:14:23 +0300

    20092023

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -102,18 +102,26 @@ class NewVisitorTest(LiveServerTestCase):
         # Удовлетворенные, они оба ложатся спать
 
     def test_layout_and_styling(self):
-        """тест макета и стилевого оформления"""
-
-        # Эдит открывает домашнюю страницу
+        # Edith goes to the home page
         self.browser.get(self.live_server_url)
         self.browser.set_window_size(1024, 768)
-        # Она замечает, что поле ввода аккуратно центрировано
+
+        # She notices the input box is nicely centered
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
-        inputbox.send_keys('testing')
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: testing')
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
             512,
-            delta=10
+            delta=5
+        )
+
+        # She starts a new list and sees the input is nicely
+        # centered there too
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
         )
