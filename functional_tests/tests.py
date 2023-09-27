73ea5396413f318b45f37d652f0eb21ff5d7884a
srkarnik <@NikitaKaravaev25>
2023-09-27 13:17:19 +0300

    27.09_2

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -104,13 +104,13 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def test_layout_and_styling(self):
         # Edith goes to the home page
         self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024, 768)
+        self.browser.set_window_size(1920, 1080)
 
         # She notices the input box is nicely centered
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
+            960,
             delta=5
         )
 
@@ -122,6 +122,6 @@ class NewVisitorTest(StaticLiveServerTestCase):
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
+            960,
             delta=5
         )
