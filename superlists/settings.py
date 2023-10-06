a1c0e45017db10f12da71d24a0091477960acabb
Nikita Karavaev <nikitakaravaev@mail.ru>
2023-10-06 17:31:08 +0300

    add_fabric

--- superlists/settings.py
+++ superlists/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = ['nikitakaravaev.ru', 'www.nikitakaravaev.ru', 'localhost', '127
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    #'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
